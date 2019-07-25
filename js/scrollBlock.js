26a9667f7f8c6948bb709362db9d4bdda3d4d5a4
fullprofit24 <50195639+fullprofit24@users.noreply.github.com>
2019-07-26 00:20:37 +0300

    Update scrollBlock.js

--- js/scrollBlock.js
+++ js/scrollBlock.js
@@ -28,7 +28,7 @@ $(function(){
 		$('html, body').animate({
 				scrollTop: (parseInt( anchors[currentAnchor] ) - 100)
 			}, {
-				duration: 12000,
+				duration: 1200,
 				specialEasing: { scrollTop: animation },
 				complete: function() { isAnimating  = false; }
 			}
